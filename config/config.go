448822308ab84232858e44a353bfc1f1f0a07d0f
GGGeorge <george.gevorkov@yandex.ru>
2024-09-09 18:31:28 +0300

    change config

--- config/config.go
+++ config/config.go
@@ -1,9 +1,6 @@
 package config
 
-import (
-	"flag"
-	"os"
-)
+import "flag"
 
 var AppConfig struct {
 	Host      string
@@ -12,15 +9,10 @@ var AppConfig struct {
 
 func init() {
 
-	if host := os.Getenv("SERVER_ADDRESS"); host != "" {
-		AppConfig.Host = host
-	} else {
-		flag.StringVar(&AppConfig.Host, "a", "http://127.0.0.1:8080", "It's a Host")
-	}
-	if baseURL := os.Getenv("BASE_URL"); baseURL != "" {
-		AppConfig.ResultURL = baseURL
-	} else {
-		flag.StringVar(&AppConfig.ResultURL, "b", "http://127.0.0.1:8080/qsd54gFg", "It's a Result URL")
-	}
+	//AppConfig.Host = "localhost:8080"
+	//AppConfig.ResultURL = "http://localhost:8080"
+
+	flag.StringVar(&AppConfig.Host, "a", "localhost:8080", "It's a Host")
+	flag.StringVar(&AppConfig.ResultURL, "b", "http://localhost:8080", "It's a Result URL")
 
 }
