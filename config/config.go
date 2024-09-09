44136ec707b5378c7ffbbb44be4712eae0828cdb
GGGeorge <george.gevorkov@yandex.ru>
2024-09-09 18:08:06 +0300

    change PORT add http://

--- config/config.go
+++ config/config.go
@@ -15,12 +15,12 @@ func init() {
 	if host := os.Getenv("SERVER_ADDRESS"); host != "" {
 		AppConfig.Host = host
 	} else {
-		flag.StringVar(&AppConfig.Host, "a", "127.0.0.1:8080", "It's a Host")
+		flag.StringVar(&AppConfig.Host, "a", "http://127.0.0.1:8080", "It's a Host")
 	}
 	if baseURL := os.Getenv("BASE_URL"); baseURL != "" {
 		AppConfig.ResultURL = baseURL
 	} else {
-		flag.StringVar(&AppConfig.ResultURL, "b", "127.0.0.1:8080/qsd54gFg", "It's a Result URL")
+		flag.StringVar(&AppConfig.ResultURL, "b", "http://127.0.0.1:8080/qsd54gFg", "It's a Result URL")
 	}
 
 }
