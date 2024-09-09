4fcebbed144dc88ef4d00b691eb15de7e974fcbf
GGGeorge <george.gevorkov@yandex.ru>
2024-09-09 18:54:33 +0300

    flags hard

--- config/config.go
+++ config/config.go
@@ -12,15 +12,13 @@ var AppConfig struct {
 
 func init() {
 
+	flag.StringVar(&AppConfig.Host, "a", "localhost:8080", "It's a Host")
+	flag.StringVar(&AppConfig.ResultURL, "b", "http://localhost:8080", "It's a Result URL")
+
 	if host := os.Getenv("SERVER_ADDRESS"); host != "" {
 		AppConfig.Host = host
-	} else {
-		flag.StringVar(&AppConfig.Host, "a", "localhost:8080", "It's a Host")
 	}
 	if baseURL := os.Getenv("BASE_URL"); baseURL != "" {
 		AppConfig.ResultURL = baseURL
-	} else {
-		flag.StringVar(&AppConfig.ResultURL, "b", "http://localhost:8080", "It's a Result URL")
 	}
-
 }
