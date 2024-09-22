5a3012a0e953f55d8bbadbce04591ec551508b84
GGGeorge <george.gevorkov@yandex.ru>
2024-09-22 15:39:06 +0300

    log zap start serv

--- go.mod
+++ go.mod
@@ -2,7 +2,12 @@ module github.com/GevorkovG/go-shortener-tlp
 
 go 1.22.5
 
-require github.com/stretchr/testify v1.9.0
+require (
+	github.com/stretchr/testify v1.9.0
+	go.uber.org/zap v1.27.0
+)
+
+require go.uber.org/multierr v1.10.0 // indirect
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
--- internal/app/run.go
+++ internal/app/run.go
@@ -2,20 +2,30 @@ package app
 
 import (
 	"flag"
-	"log"
 	"net/http"
 
 	"github.com/GevorkovG/go-shortener-tlp/config"
-
 	"github.com/go-chi/chi"
+	"go.uber.org/zap"
 )
 
 func Run() {
+
+	logger, err := zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync() // flushes buffer, if any
+
+	sugar := *logger.Sugar()
+
 	r := chi.NewRouter()
 	r.Post("/", GetShortURL)
 	r.Get("/{id}", GetOriginURL)
 
 	flag.Parse()
 
-	log.Fatal(http.ListenAndServe(config.AppConfig.Host, r))
+	if err := http.ListenAndServe(config.AppConfig.Host, r); err != nil {
+		sugar.Fatalw(err.Error(), "event", "start server")
+	}
 }
