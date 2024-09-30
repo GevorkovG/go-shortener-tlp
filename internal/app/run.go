d0032358b5ab1d42bb412361b6542bfb6e3f52e8
GGGeorge <george.gevorkov@yandex.ru>
2024-10-01 00:22:38 +0300

    added internal/storage

--- internal/app/run.go
+++ internal/app/run.go
@@ -1,52 +1,25 @@
 package app
 
 import (
-	"errors"
 	"flag"
+	"log"
 	"net/http"
 
 	"github.com/GevorkovG/go-shortener-tlp/config"
-	"github.com/GevorkovG/go-shortener-tlp/internal/log"
+	logg "github.com/GevorkovG/go-shortener-tlp/internal/log"
 	"github.com/go-chi/chi"
 )
 
-type Storage struct {
-	urls map[string]string
-}
-
-func NewStorage() *Storage {
-	return &Storage{
-		urls: make(map[string]string),
-	}
-}
-
-func (s *Storage) SetURL(key, value string) {
-	s.urls[key] = value
-}
-
-func (s *Storage) GetURL(key string) (string, error) {
-
-	url, ok := s.urls[key]
-	if ok {
-		return url, nil
-	}
-	return "", errors.New("id not found")
-}
-
-type AppConfig struct {
-	Host      string
-	ResultURL string
-}
 type App struct {
-	cfg     *AppConfig
-	storage *Storage
+	cfg     *config.AppConfig
+	storage *config.Storage
 }
 
-func NewApp(cfg *AppConfig) *App {
+func NewApp(cfg *config.AppConfig) *App {
 
 	return &App{
 		cfg:     cfg,
-		storage: NewStorage(),
+		storage: config.NewStorage(),
 	}
 }
 
@@ -54,8 +27,8 @@ func Run() {
 	conf := config.NewCfg()
 
 	r := chi.NewRouter()
-	r.Post("/", log.WithLogging(conf.GetShortURL))
-	r.Get("/{id}", log.WithLogging(conf.GetOriginURL))
+	r.Post("/", logg.WithLogging(conf.GetShortURL))
+	r.Get("/{id}", logg.WithLogging(conf.GetOriginURL))
 	flag.Parse()
 
 	log.Fatal(http.ListenAndServe(conf.Host, r))
