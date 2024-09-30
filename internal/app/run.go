f670547a184974b395b28147f83e20906fbae29c
GGGeorge <george.gevorkov@yandex.ru>
2024-10-01 00:39:24 +0300

    app/run cfg repaired

--- internal/app/run.go
+++ internal/app/run.go
@@ -7,30 +7,31 @@ import (
 
 	"github.com/GevorkovG/go-shortener-tlp/config"
 	logg "github.com/GevorkovG/go-shortener-tlp/internal/log"
+	"github.com/GevorkovG/go-shortener-tlp/internal/storage"
 	"github.com/go-chi/chi"
 )
 
 type App struct {
 	cfg     *config.AppConfig
-	storage *config.Storage
+	storage *storage.Storage
 }
 
 func NewApp(cfg *config.AppConfig) *App {
 
 	return &App{
 		cfg:     cfg,
-		storage: config.NewStorage(),
+		storage: storage.NewStorage(),
 	}
 }
 
 func Run() {
+	var cfg *config.AppConfig
 	conf := config.NewCfg()
-
+	newApp := NewApp(cfg)
 	r := chi.NewRouter()
-	r.Post("/", logg.WithLogging(conf.GetShortURL))
-	r.Get("/{id}", logg.WithLogging(conf.GetOriginURL))
+	r.Post("/", logg.WithLogging(newApp.GetShortURL))
+	r.Get("/{id}", logg.WithLogging(newApp.GetOriginURL))
 	flag.Parse()
-
 	log.Fatal(http.ListenAndServe(conf.Host, r))
 
 }
