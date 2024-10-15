f2ff1d9f581305fc7885d841976a36f4eb965ed1
GGGeorge <george.gevorkov@yandex.ru>
2024-10-16 02:58:59 +0300

    caarlos0/env used in config

--- config/config.go
+++ config/config.go
@@ -3,8 +3,8 @@ package config
 import (
 	"flag"
 	"fmt"
-	"os"
-	"strings"
+
+	"github.com/caarlos0/env"
 )
 
 const (
@@ -15,10 +15,10 @@ const (
 )
 
 type AppConfig struct {
-	Host           string
-	ResultURL      string
-	FilePATH       string
-	DataBaseString string
+	Host           string `env:"SERVER_ADDRESS"`
+	ResultURL      string `env:"BASE_URL"`
+	FilePATH       string `env:"FILE_STORAGE_PATH"`
+	DataBaseString string `env:"DATABASE_DSN"`
 }
 
 func NewCfg() *AppConfig {
@@ -33,27 +33,12 @@ func NewCfg() *AppConfig {
 	flag.StringVar(&a.FilePATH, "f", "/tmp/shortener-db.json", "It's a FilePATH")
 	flag.StringVar(&a.DataBaseString, "d", ps, "it's conn string")
 
-	if host := os.Getenv("SERVER_ADDRESS"); host != "" {
-		a.Host = strings.TrimSpace(host)
-	}
-	if baseURL := os.Getenv("BASE_URL"); baseURL != "" {
-		a.ResultURL = strings.TrimSpace(baseURL)
-	}
-	if baseURL := os.Getenv("FILE_STORAGE_PATH"); baseURL != "" {
-		a.FilePATH = strings.TrimSpace(baseURL)
-	}
-	if baseURL := os.Getenv("BASE_URL"); baseURL != "" {
-		a.ResultURL = strings.TrimSpace(baseURL)
-	}
-	if host := os.Getenv("SERVER_ADDRESS"); host != "" {
-		a.Host = strings.TrimSpace(host)
-	}
-	if file := os.Getenv("FILE_STORAGE_PATH"); file != "" {
-		a.FilePATH = file
-	}
-	if datab := os.Getenv("DATABASE_DSN"); datab != "" {
-		a.DataBaseString = datab
-	}
 	flag.Parse()
+
+	err := env.Parse(a)
+	if err != nil {
+		panic(err)
+	}
+
 	return a
 }
