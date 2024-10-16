d8050ae16434690f8cf3261e8dfa87043478c71b
GGGeorge <george.gevorkov@yandex.ru>
2024-10-16 13:50:24 +0300

    upd JSONGetShortURL GetOriginURL

--- internal/app/shortener.go
+++ internal/app/shortener.go
@@ -41,6 +41,15 @@ func (l *Link) Insert(link *Link) (*Link, error) {
 	return link, nil
 }
 
+func (l *Link) GetOriginal(short string) (*Link, error) {
+
+	if err := l.Store.DB.QueryRow("SELECT original FROM links WHERE short = $1", short).Scan(&l.Original); err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	return l, nil
+}
+
 func generateID() string {
 	letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 	b := make([]rune, 8)
@@ -50,17 +59,17 @@ func generateID() string {
 	return string(b)
 }
 
-type Request1 struct {
+type Request struct {
 	URL string `json:"url"`
 }
 
-type Response1 struct {
+type Response struct {
 	Result string `json:"result"`
 }
 
 func (a *App) JSONGetShortURL(w http.ResponseWriter, r *http.Request) {
 
-	var req Request1
+	var req Request
 
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
@@ -70,20 +79,38 @@ func (a *App) JSONGetShortURL(w http.ResponseWriter, r *http.Request) {
 
 	id := generateID()
 
-	a.Storage.SetURL(id, req.URL)
-	fileStorage := storage.NewFileStorage()
-
-	fileStorage.Short = id
-	fileStorage.Original = req.URL
-
-	err = storage.SaveToFile(fileStorage, a.cfg.FilePATH)
-	if err != nil {
-		log.Println(err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+	linkModel := Link{
+		Short:    id,
+		Original: req.URL,
+		Store:    a.DataBase,
 	}
 
-	result := Response1{
+	if a.DBReady {
+		_, err = linkModel.Insert(&linkModel)
+		if err != nil {
+			log.Println("Don't insert url!")
+			log.Println(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	} else if a.cfg.FilePATH != "" {
+
+		fileStorage := storage.NewFileStorage()
+
+		fileStorage.Short = id
+		fileStorage.Original = req.URL
+
+		err = storage.SaveToFile(fileStorage, a.cfg.FilePATH)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	a.Storage.SetURL(id, req.URL)
+
+	result := Response{
 		Result: a.cfg.ResultURL + "/" + id,
 	}
 
@@ -175,11 +202,25 @@ func (a *App) GetOriginURL(w http.ResponseWriter, r *http.Request) {
 
 	id := chi.URLParam(r, "id")
 
-	url, err := a.Storage.GetURL(id)
-
-	if err != nil {
-		http.Error(w, "Invalid URL", http.StatusBadRequest)
+	linkModel := &Link{
+		Store: a.DataBase,
 	}
-	w.Header().Set("Location", url)
+
+	var err error
+	if a.DBReady {
+		linkModel, err = linkModel.GetOriginal(id)
+		if err != nil {
+			log.Println("Didn't read data from table")
+			log.Println(err)
+			http.Error(w, "Invalid URL", http.StatusBadRequest)
+		}
+	}
+	if linkModel.Original == "" {
+		linkModel.Original, err = a.Storage.GetURL(id)
+		if err != nil {
+			http.Error(w, "Invalid URL", http.StatusBadRequest)
+		}
+	}
+	w.Header().Set("Location", linkModel.Original)
 	w.WriteHeader(http.StatusTemporaryRedirect)
 }
