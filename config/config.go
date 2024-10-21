da7e50c98b2d6e2d26c417cd9fbc5cf2831be708
GGGeorge <george.gevorkov@yandex.ru>
2024-10-21 13:25:39 +0300

    confit using caarlos0

--- config/config.go
+++ config/config.go
@@ -2,8 +2,8 @@ package config
 
 import (
 	"flag"
-	"os"
-	"strings"
+
+	"github.com/caarlos0/env"
 )
 
 /*
@@ -16,48 +16,27 @@ const (
 */
 
 type AppConfig struct {
-	Host           string
-	ResultURL      string
-	FilePATH       string
-	DataBaseString string
+	Host           string `env:"SERVER_ADDRESS"`
+	ResultURL      string `env:"BASE_URL"`
+	FilePATH       string `env:"FILE_STORAGE_PATH"`
+	DataBaseString string `env:"DATABASE_DSN"`
 }
 
 func NewCfg() *AppConfig {
 
-	a := &AppConfig{}
-
-	//	ps := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable",
-	//		DBhost, DBuser, DBpassword, DBdbname)
+	a := AppConfig{}
 
 	flag.StringVar(&a.Host, "a", "localhost:8080", "It's a Host")
 	flag.StringVar(&a.ResultURL, "b", "http://localhost:8080", "It's a Result URL")
 	flag.StringVar(&a.FilePATH, "f", "", "It's a FilePATH")
 	flag.StringVar(&a.DataBaseString, "d", "", "it's conn string")
-	//flag.StringVar(&a.DataBaseString, "d", ps, "it's conn string")
-	//flag.StringVar(&a.FilePATH, "f", "/tmp/shortener-db.json", "It's a FilePATH")
 
-	if host := os.Getenv("SERVER_ADDRESS"); host != "" {
-		a.Host = strings.TrimSpace(host)
-	}
-	if baseURL := os.Getenv("BASE_URL"); baseURL != "" {
-		a.ResultURL = strings.TrimSpace(baseURL)
-	}
-	if baseURL := os.Getenv("FILE_STORAGE_PATH"); baseURL != "" {
-		a.FilePATH = strings.TrimSpace(baseURL)
-	}
-	if baseURL := os.Getenv("BASE_URL"); baseURL != "" {
-		a.ResultURL = strings.TrimSpace(baseURL)
-	}
-	if host := os.Getenv("SERVER_ADDRESS"); host != "" {
-		a.Host = strings.TrimSpace(host)
-	}
-	if file := os.Getenv("FILE_STORAGE_PATH"); file != "" {
-		a.FilePATH = file
-	}
-	if datab := os.Getenv("DATABASE_DSN"); datab != "" {
-		a.DataBaseString = datab
-	}
 	flag.Parse()
 
-	return a
+	err := env.Parse(&a)
+	if err != nil {
+		panic(err)
+	}
+
+	return &a
 }
--- go.mod
+++ go.mod
@@ -20,6 +20,7 @@ require (
 )
 
 require (
+	github.com/caarlos0/env v3.5.0+incompatible
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-chi/chi v1.5.5
 	github.com/pmezard/go-difflib v1.0.0 // indirect
