8d6c7206ca4900bc38e44e8f43d080958958620c
GevorkovG <george.gevorkov@yandex.ru>
2024-12-15 18:55:08 +0300

    add cookie,jwtbuild package

---
+++ internal/app/services/jwtstring/jwtstring.go
@@ -0,0 +1,39 @@
+package BuildJWTString
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+// Claims — структура утверждений, которая включает стандартные утверждения
+// и одно пользовательское — UserID
+type Claims struct {
+	jwt.RegisteredClaims
+	UserID string
+}
+
+const TOKEN_EXP = time.Hour * 3
+const SECRET_KEY = "sHoRtEnEr"
+
+// BuildJWTString создаёт токен и возвращает его в виде строки.
+func BuildJWTString(uuid string) (string, error) {
+	// создаём новый токен с алгоритмом подписи HS256 и утверждениями — Claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, Claims{
+		RegisteredClaims: jwt.RegisteredClaims{
+			// когда создан токен
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(TOKEN_EXP)),
+		},
+		// собственное утверждение
+		UserID: uuid,
+	})
+
+	// создаём строку токена
+	tokenString, err := token.SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		return "", err
+	}
+
+	// возвращаем строку токена
+	return tokenString, nil
+}
---
+++ internal/app/services/usertoken/cookie.go
@@ -0,0 +1,47 @@
+package usertoken
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/GevorkovG/go-shortener-tlp/internal/services/jwtstring"
+	"github.com/golang-jwt/jwt"
+)
+
+func GetUserId(tokenString string) (string, error) {
+	claims := &jwtstring.Claims{}
+	_, err := jwt.ParseWithClaims(tokenString, claims,
+		func(t *jwt.Token) (interface{}, error) {
+			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+			}
+			return []byte(jwtstring.SECRET_KEY), nil
+		})
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
+
+	return claims.UserID, nil
+}
+
+func ValidationToken(tokenString string) bool {
+
+	claims := &jwtstring.Claims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims,
+		func(t *jwt.Token) (interface{}, error) {
+			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+			}
+			return []byte(jwtstring.SecretKey), nil
+		})
+	if err != nil {
+		return false
+	}
+
+	if !token.Valid {
+		return false
+	}
+
+	return true
+}
