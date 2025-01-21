ada870fce885e699e654f0cdca30528691e8fa26
GevorkovG <george.gevorkov@yandex.ru>
2025-01-21 08:33:15 +0300

    fix w.Header().Set in urls

--- internal/app/urls.go
+++ internal/app/urls.go
@@ -18,7 +18,9 @@ func (a *App) APIGetUserURLs(w http.ResponseWriter, r *http.Request) {
 	userID, ok := r.Context().Value(cookies.ContextUserKey).(string)
 	if !ok || userID == "" {
 		zap.L().Warn("Unauthorized access attempt")
+		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(map[string]string{"error": "Unauthorized"})
 		return
 	}
 	zap.L().Info("UserID extracted from context", zap.String("userID", userID))
@@ -27,14 +29,16 @@ func (a *App) APIGetUserURLs(w http.ResponseWriter, r *http.Request) {
 	userURLs, err := a.Storage.GetAllByUserID(userID)
 	if err != nil {
 		zap.L().Error("Failed to get user URLs", zap.String("userID", userID), zap.Error(err))
+		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"error": "Internal Server Error"})
 		return
 	}
 	zap.L().Info("User URLs retrieved from storage", zap.String("userID", userID), zap.Any("userURLs", userURLs))
 
 	if len(userURLs) == 0 {
 		zap.L().Info("No URLs found for user", zap.String("userID", userID))
-		w.WriteHeader(http.StatusNoContent)
+		w.WriteHeader(http.StatusNoContent) // Возвращаем 204, если данных нет
 		return
 	}
 
@@ -47,16 +51,9 @@ func (a *App) APIGetUserURLs(w http.ResponseWriter, r *http.Request) {
 		})
 	}
 
-	response, err := json.Marshal(links)
-	if err != nil {
-		zap.L().Error("Failed to marshal user URLs", zap.String("userID", userID), zap.Error(err))
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	if _, err := w.Write(response); err != nil {
+	if err := json.NewEncoder(w).Encode(links); err != nil {
 		zap.L().Error("Failed to write response", zap.Error(err))
 	}
 }
