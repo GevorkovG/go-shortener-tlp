e120242e48f77334ce03379544bb583ab2a14cdd
GevorkovG <george.gevorkov@yandex.ru>
2025-07-09 21:02:24 +0300

    add build|staticlint

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/GevorkovG/go-shortener-tlp/internal/app"
 	"github.com/GevorkovG/go-shortener-tlp/internal/log"
 	"go.uber.org/zap"
 )
 
+var (
+	buildVersion string = "N/A"
+	buildDate    string = "N/A"
+	buildCommit  string = "N/A"
+)
+
 func main() {
+
+	fmt.Printf("Build version: %s\n", buildVersion)
+	fmt.Printf("Build date: %s\n", buildDate)
+	fmt.Printf("Build commit: %s\n", buildCommit)
+
 	log.InitLogger()     // Инициализация логгера
 	defer zap.L().Sync() // Очистка буферов логгера при завершении программы
 
